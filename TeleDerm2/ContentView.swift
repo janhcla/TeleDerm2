a4727b27c1edcb35a9f739b379b1fecd0852dc59
janhcla <95102948+janhcla@users.noreply.github.com>
2023-09-19 19:18:46 +0200

    Update ContentView.swift
    Signed-off-by: janhcla <95102948+janhcla@users.noreply.github.com>

--- TeleDerm2/ContentView.swift
+++ TeleDerm2/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var showPhotoLibrary = false
     @State private var isCameraViewPresented = false
     @State private var images: [CustomImage] = []
+    @State private var capturedImage: UIImage? = nil  // New line to store the captured image
     
     @Binding var isCameraPresentedFromDeepLink: Bool
     
@@ -29,8 +30,13 @@ struct ContentView: View {
                         Text("Take a picture")
                     }
                     .sheet(isPresented: $isCameraViewPresented) {
-                        ModernCameraView(image: $images)
+                        ModernCameraView(image: $capturedImage)  // Modified line
                             .environmentObject(imageStore)
+                            .onDisappear {
+                                if let captured = capturedImage {
+                                    imageStore.add(captured)
+                                }
+                            }
                     }
                     .padding()
                 }
@@ -89,97 +95,3 @@ extension Notification.Name {
     static let didTakePhoto = Notification.Name("didTakePhoto")
     static let didSelectPhoto = Notification.Name("didSelectPhoto")
 }
-
-
-//// ContentView.swift
-//// TeleDerm2
-////
-//// Created by Jan H. Clausen on 14/04/2023.
-////
-//
-//import SwiftUI
-//
-//struct ContentView: View {
-//    @StateObject private var imageStore = ImageStore()
-//    @State private var hasImage = false
-//    @State private var showPhotoLibrary = false
-//    @State private var isCameraViewPresented = false
-//    @State private var images: [CustomImage] = []
-//
-//    @Binding var isCameraPresentedFromDeepLink: Bool
-//
-//    var body: some View {
-//        NavigationView {
-//            VStack {
-//                Button(action: {
-//                    isCameraViewPresented = true
-//
-//                }) {
-//                    VStack {
-//                        Image(systemName: "camera.fill")
-//                            .resizable()
-//                            .frame(width: 80, height: 60)
-//                        Text("Take a picture")
-//                    }
-//                    .sheet(isPresented: $isCameraViewPresented) {
-//                        ModernCameraView(images: $images)
-//                            .environmentObject(imageStore)
-//                    }
-//                    .padding()
-//                }
-//
-//                Button(action: {
-//                    showPhotoLibrary.toggle()
-//                }) {
-//                    VStack {
-//                        Image(systemName: "photo.fill")
-//                            .resizable()
-//                            .frame(width: 80, height: 60)
-//                        Text("Choose from library")
-//                    }
-//                }
-//                .padding()
-//                .fullScreenCover(isPresented: $showPhotoLibrary) {
-//                    PhotoLibraryView(images: $imageStore.images, hasImage: $hasImage).environmentObject(imageStore)
-//                }
-//
-//                if hasImage {
-//                    NavigationLink(destination: PhotosView(images: $imageStore.images).environmentObject(imageStore)) {
-//                        VStack {
-//                            Image(systemName: "photo.on.rectangle.fill")
-//                                .resizable()
-//                                .frame(width: 80, height: 60)
-//                            Text("Continue to view the photos")
-//                        }
-//                    }
-//                    .foregroundColor(Color.green)
-//                    .padding()
-//                }
-//            }
-//
-//                .onAppear {
-//                    if isCameraPresentedFromDeepLink {
-//                        isCameraViewPresented = true
-//                        isCameraPresentedFromDeepLink = false
-//                    }
-//                }
-//                .onChange(of: imageStore.images) { _ in
-//                    if imageStore.images.isEmpty {
-//                        hasImage = false
-//                    } else {
-//                        hasImage = true
-//                    }
-//                }
-//                .navigationBarItems(trailing: NavigationLink(destination: SettingsView()) {
-//                    Image(systemName: "gear")
-//                })
-//                .navigationBarBackButtonHidden(true)
-//
-//        }
-//    }
-//}
-//extension Notification.Name {
-//    static let didTakePhoto = Notification.Name("didTakePhoto")
-//    static let didSelectPhoto = Notification.Name("didSelectPhoto")
-//}
-//
